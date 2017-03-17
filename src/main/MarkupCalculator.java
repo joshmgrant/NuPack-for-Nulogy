d17a054789b7ff9e28a6c39bcd617367e6556e5e
Joshua Grant <joshua.m.grant@gmail.com>
2017-03-17 11:26:20 -0400

    started with tests based on an example from the given problem

--- src/main/MarkupCalculator.java
+++ src/main/MarkupCalculator.java
@@ -2,5 +2,35 @@ package main;
 
 public class MarkupCalculator {
 
+	private double basePrice;
+	private int peopleNeeded;
+	
+	public enum MaterialsType {
+		FOOD,
+		PHARMACEUTICALS,
+		BOOKS,
+		ELECTRONICS
+	}
+	
+	private MaterialsType materialsType;
+	
+	public void setBasePrice(double basePrice) {
+		this.basePrice = basePrice;
+	}
+
+	public void setPeopleNeeded(int peopleNeeded) {
+		this.peopleNeeded = peopleNeeded;
+	}
+	
+	public void setMaterialsType(MaterialsType type){
+		this.materialsType = type;
+	}
+	
+	public double getFinalCost() {
+		return this.basePrice;
+	}
+	
+	
+
 	
 }
--- src/test/TestMarkupCalculator.java
+++ src/test/TestMarkupCalculator.java
@@ -1,15 +1,23 @@
 package test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import main.MarkupCalculator;
+import main.MarkupCalculator.MaterialsType;
 
 public class TestMarkupCalculator {
-
+	
 	@Test
-	public void testBasic() {
-		fail("Not implemented");
+	public void testExampleCalculation() {
+		MarkupCalculator calculator = new MarkupCalculator();
+		
+		calculator.setBasePrice(5432.00); 
+		calculator.setPeopleNeeded(1);
+		calculator.setMaterialsType(MaterialsType.PHARMACEUTICALS);
+		
+		assertEquals(calculator.getFinalCost(), 6199.81, 0.001);
 	}
+	
 
 }
