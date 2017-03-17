630daf82ac17f7e3d16717ef86f9f9f7bc57b05e
Joshua Grant <joshua.m.grant@gmail.com>
2017-03-17 10:19:56 -0400

    start with a single failing test

---
+++ src/test/TestMarkupCalculator.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+import main.MarkupCalculator;
+
+public class TestMarkupCalculator {
+
+	@Test
+	public void testBasic() {
+		fail("Not implemented");
+	}
+
+}
